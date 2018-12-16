3f8a9dd34cacdb4900eb5bdf4ee2a3945c66b267
soplar <joan.vanduren@gmail.com>
2018-12-16 15:23:23 +0100

    aanpassing db config routes.py

--- routes.py
+++ routes.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+# importeer modelsfunctionaliteit
+from models import db
 # 
 app = Flask(__name__)
 
 # database connectie
 # eerst: pip install flask-alchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
+# initialiseer de database setup
+db.init_app(app)
 
 @app.route("/")
 def index():
